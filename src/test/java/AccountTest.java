908a0a8caf19f8a35bd5b68ac641426370223d96
Jeremy <jeremy.montanes@external.grtgaz.com>
2022-09-12 13:52:01 +0200

    we can print all operations from an account

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,6 +1,4 @@
-import kata.Account;
-import kata.Amount;
-import kata.Balance;
+import kata.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,16 +7,20 @@ import java.math.BigDecimal;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 public class AccountTest {
 
     private Account account;
     Clock fixed = Clock.fixed(Instant.now(), ZoneId.systemDefault());
     private final Amount ten = new Amount(BigDecimal.TEN);
+    private final StatementPrinterFake printer = new StatementPrinterFake();
+    private final ZonedDateTime date = ZonedDateTime.now(fixed);
 
     @BeforeEach
     void init_BankAccount(){
-        account = new Account(fixed);
+        account = new Account(fixed,printer);
     }
 
     @Test
@@ -39,6 +41,20 @@ public class AccountTest {
     @Test
     public void we_cant_withdrawal_10_from_an_empty_account() {
         Assertions.assertThrows(IllegalArgumentException.class, () ->  account.withdrawal(ten));
+    }
+
+    @Test
+    public void when_we_deposit_10_whe_should_get_1_statement_at_print() {
+        Statement expect = Statement.builder()
+                .type(OperationType.DEPOSIT)
+                .date(date)
+                .amount(ten)
+                .balance(new Balance(BigDecimal.TEN))
+                .build();
+        account.deposit(ten);
+        account.print();
+        List<Statement> statementList = printer.getStatements();
+        Assertions.assertEquals(expect, statementList.get(0));
     }
 }
 
